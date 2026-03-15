e5bdd1bd9793c2f9777a6312da136df937d382bd
liuxuead <839310438@qq.com>
2026-03-15 19:13:24 +0800

    Add files via upload

--- game.js
+++ game.js
@@ -60,7 +60,7 @@ class FishHunterGame {
     
     this.playerLevel = 0;
     this.levelThresholds = [0, 10, 50, 100, 200, 400, 600, 900, 1300, 1600, 2000];
-    this.bigFishPerLevel = [3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+    this.bigFishPerLevel = [3, 4, 4, 8, 8, 10, 10, 15, 15, 20, 20];
     this.bigFishCurrentTarget = 0;
     
     this.maxFishPerLevel = [5, 5, 8, 10, 20, 30, 40, 40, 40, 40, 40];
